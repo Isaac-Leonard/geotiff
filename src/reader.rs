b7f88a3e5602eba1e6420b17566a54438f812564
Isaac Leonard <c3330849@uon.edu.au>
2023-08-10 01:17:17 +1000

    Fix bug

--- src/reader.rs
+++ src/reader.rs
@@ -436,11 +436,11 @@ impl TIFFReader {
             let tile_row = nth_tile / tiles_across;
             let start_x = tile_col * tile_width;
             let mut curr_x = start_x;
-            let _end_x = (tile_col + 1) * tile_width;
+            let end_x = (tile_col + 1) * tile_width;
             let max_y = tiles_down * tile_length;
             let start_y = max_y - (tile_row + 1) * tile_length;
             let mut curr_y = start_y;
-            let end_y = max_y - tile_row * tile_length;
+            let _end_y = max_y - tile_row * tile_length;
             reader.seek(SeekFrom::Start(*offset as u64))?;
             for _i in 0..(*byte_count / image_depth as u32) {
                 let v = self.read_n(reader, image_depth as u64);
